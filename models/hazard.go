e402f5b4ddb99982d7156f3188c9947d24ecef5e
krassnigg@babymarkt.de <krassnigg@babymarkt.de>
2019-02-22 08:59:25 +0100

    Finish first draft of database migrations

--- models/hazard.go
+++ models/hazard.go
@@ -11,13 +11,19 @@ type Hazard struct {
 	ID          uuid.UUID `db:"id"`
 	Label       string    `db:"label"`
 	Description string    `db:"description"`
-	Latitude    float32   `db:"latitude"`
-	Longitude   float32   `db:"longitude"`
+	Location    Geography `db:"location"`
+	Visible     bool      `db:"visible"`
 	User        User      `belongs_to:"user"`
 	UserID      uuid.UUID `db:"user_id"`
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
 }
 
-// Hazards is a collection of road hazards
+// Hazards is an array of road hazards
 type Hazards []Hazard
+
+// Geography describes a geographical point
+type Geography struct {
+	Latitude  float32
+	Longitude float32
+}
--- models/user.go
+++ models/user.go
@@ -1,12 +1,18 @@
 package models
 
-import "github.com/gofrs/uuid"
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
 
 // User represents a single account with access to the application
 type User struct {
-	ID       uuid.UUID `db:"id"`
-	Name     string    `db:"name"`
-	Email    string    `db:"email"`
-	Password string    `db:"password"`
-	Hazards  Hazards   `has_many:"hazards" order_by:"updated_at desc"`
+	ID        uuid.UUID `db:"id"`
+	Name      string    `db:"name"`
+	Email     string    `db:"email"`
+	Password  string    `db:"password"`
+	Hazards   Hazards   `has_many:"hazards" order_by:"updated_at desc"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }

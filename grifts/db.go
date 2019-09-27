5b7657df1ebbb254d7b2812ea5781cf9381ebc5b
krassnigg@babymarkt.de <krassnigg@babymarkt.de>
2019-09-27 13:45:59 +0200

    Create admin user for testing in seed task

--- grifts/db.go
+++ grifts/db.go
@@ -1,14 +1,34 @@
 package grifts
 
 import (
+	"errors"
+
+	"github.com/gobuffalo/pop"
 	"github.com/markbates/grift/grift"
+	"github.com/tardog/schutzstreifen/models"
 )
 
 var _ = grift.Namespace("db", func() {
 
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
+		tx, err := pop.Connect("development")
+		if err != nil {
+			return err
+		}
+
+		user := &models.User{
+			Name:     "admin",
+			Email:    "admin@schutzstreifen.dev",
+			Password: "dirtysecret",
+			Admin:    true,
+		}
+
+		verrs, err := tx.ValidateAndCreate(user)
+		if err != nil || verrs.HasAny() {
+			return errors.New("failed to create the admin user")
+		}
+
 		return nil
 	})
 

4561465ab7a982594ea3fa6dcbe34df6747fb2ab
krassnigg@babymarkt.de <krassnigg@babymarkt.de>
2019-11-29 10:04:52 +0100

    Test the points handler

--- actions/home_test.go
+++ actions/home_test.go
@@ -1,7 +1,20 @@
 package actions
 
+// Test that the / route loads successfully
 func (as *ActionSuite) Test_HomeHandler() {
 	res := as.HTML("/").Get()
 	as.Equal(200, res.Code)
 	as.Contains(res.Body.String(), "main-map")
+}
+
+// Test that the /points route returns a JSON representation of all hazards
+func (as ActionSuite) Test_PointsHandler() {
+	as.LoadFixture("hazard types")
+	as.LoadFixture("users")
+	as.LoadFixture("hazards")
+
+	res := as.JSON("/points").Get()
+	as.Equal(200, res.Code)
+
+	as.Contains(res.Body.String(), string(`"id":"1111a456-3e1d-482d-85c3-5eee7df8816e"`))
 }
